ec6b875a14917d639acb951264c58791bc90ccd9
Ahmedshahid1400 <ahmedshahid1400@gmail.com>
2026-06-25 00:02:56 +0300

    chaged to env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: "postgres-db",
-  user: "postgres",
-  password: "12345",
-  database: "cloudcart",
-  port: 5432,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 module.exports = pool;
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 
@@ -10,6 +12,8 @@ app.use(express.json());
 
 app.use("/products", productRoutes);
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
